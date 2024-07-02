7cccfa6db8cdcdae9d38165332119b5570388ef3
Sid Mohan <sidmohan@Sids-MacBook-Pro.local>
2024-07-02 08:30:00 -0700

    updated setup.py

--- setup.py
+++ setup.py
@@ -2,19 +2,18 @@ from setuptools import setup, find_packages
 
 setup(
     name="datafog_instructor",
-    version="0.1.0b1",
+    version="0.1.0b2",
     packages=find_packages(),
     install_requires=[
         "pydantic",
         "ollama",
-        "ollama_instructor",
+        "ollama-instructor",
         "python-dotenv"
-
     ],
     author="Sid Mohan",
     author_email="sid@datafog.ai",
     description="A brief description of datafog_instructor",
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/datafog/datafog-python",
+    url="https://github.com/datafog/datafog-instructor",
 )
